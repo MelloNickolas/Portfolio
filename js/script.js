a6f156eb2203dd266f5ffbde9b075e398eaa810d
Nickolas Passos Homem de Mello <nickolasphmello10@gmail.com>
2025-03-10 00:27:19 -0300

    Remove url animations

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ import "./PullDados.js"
 import "./scrollanimations.js";
 import "./modalSobre.js";
 import "./backToTop.js";
-import "./uirlanimations.js";
 
 console.log(`%c
   ███╗   ██╗██╗ ██████╗██╗  ██╗ ██████╗ ██╗      █████╗ ███████╗
